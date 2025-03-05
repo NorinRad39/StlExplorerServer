3533b74035c0336f05c309bc347a6ec5b2264ccf
NorinRad39 <89634711+NorinRad39@users.noreply.github.com>
2025-03-05 23:48:41 +0100

    connexion bdd ok normalement

---
+++ StlExplorerServer/Migrations/20250305224455_InitialCreate_v2.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StlExplorerServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate_v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Famille",
+                columns: table => new
+                {
+                    FamilleID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NomFamille = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Famille", x => x.FamilleID);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Sujet",
+                columns: table => new
+                {
+                    SujetID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NomSujet = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    FamilleID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sujet", x => x.SujetID);
+                    table.ForeignKey(
+                        name: "FK_Sujet_Famille_FamilleID",
+                        column: x => x.FamilleID,
+                        principalTable: "Famille",
+                        principalColumn: "FamilleID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Packets",
+                columns: table => new
+                {
+                    PacketID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SujetID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Packets", x => x.PacketID);
+                    table.ForeignKey(
+                        name: "FK_Packets_Sujet_SujetID",
+                        column: x => x.SujetID,
+                        principalTable: "Sujet",
+                        principalColumn: "SujetID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Packets_SujetID",
+                table: "Packets",
+                column: "SujetID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sujet_FamilleID",
+                table: "Sujet",
+                column: "FamilleID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Packets");
+
+            migrationBuilder.DropTable(
+                name: "Sujet");
+
+            migrationBuilder.DropTable(
+                name: "Famille");
+        }
+    }
+}
